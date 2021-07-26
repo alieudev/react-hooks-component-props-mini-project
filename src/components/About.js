17a9a93274d4b728c04cf93cbae6d29a6a1a3a78
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-26 10:35:14 -0400

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+function About({src = 'https://via.placeholder.com/215', about}){
+    return(<aside>
+        <img src={src} alt="blog logo"/>
+        <p>{about}</p>
+    </aside>)
+}
+
+export default About; 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+function Header({name}){
+    return (<div>
+        <h1>{name}</h1>
+        </div>)
+}
+
+export default Header;
